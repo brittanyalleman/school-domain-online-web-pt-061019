e94917e07033603c9cde505b5e61576d7b209851
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-18 03:09:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+#require "pry"
+class School
+  attr_accessor :roster, :grade
+ def initialize(name)
+   @roster = {}
+   @name= name
+ end
+ def add_student(name, grade)
+   if !roster[grade]
+     roster[grade] = []
+   end
+roster[grade] << name
+end
+def grade(grade)
+  roster[grade]
+end
+ def roster(name)
+   roster.sort
+ end
+   #if grade isn't in roster ref to line 9
+   #if there is a key value pair that corrisponds with the grade, then just add the name to the value array
+   # how do determine if the hash has a particular key
+
+
+end
